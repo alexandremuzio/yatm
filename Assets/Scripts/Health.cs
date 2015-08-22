1cea921d19315c0d86bb6d49136d225ea6788d71
ghust1995 <gustavo.ceci95@gmail.com>
2015-08-22 16:27:29 -0300

    Added health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+ 
+    [SerializeField]
+    private float maxHealth = 100;
+    private float currentHealth;
+    public float GetLifeRatio()
+    {
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        return currentHealth / maxHealth;
+    }
+
+    private SpriteRenderer healthImage;
+    private SpriteRenderer healthBar;
+
+    [SerializeField]
+    private float offset = -300;
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        currentHealth = maxHealth;
+        healthBar = transform.Find("lifeBar").GetComponent<SpriteRenderer>();
+        healthImage = transform.Find("lifeImage").GetComponent<SpriteRenderer>();      
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (transform.rotation != Quaternion.identity)
+            transform.rotation = Quaternion.identity;
+        transform.position = new Vector2(transform.parent.transform.position.x, transform.parent.transform.position.y + offset);
+
+        healthImage.transform.localScale = new Vector2(GetLifeRatio(), healthImage.transform.localScale.y);
+	}
+
+    [ContextMenu("Test Damage!")]
+    private void Damage()
+    {
+        Debug.Log("Damaged: " + 20);
+        Damage(20);
+    }
+    public void Damage(float dmg)
+    {
+        currentHealth -= dmg;
+    }
+}
--- Assets/Scripts/Vibrator.cs
+++ Assets/Scripts/Vibrator.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 public class Vibrator1 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    IEnumerator WaitAndPrint()
+    {
+        yield return new WaitForSeconds(1);
+        GamePad.SetVibration(0, 0.33f, 0.33f);
+        yield return new WaitForSeconds(1);
+        GamePad.SetVibration(0, 0f, 0f);
+        yield return new WaitForSeconds(1);
+        GamePad.SetVibration(0, 0.67f, 0.67f);
+        yield return new WaitForSeconds(1);
+        GamePad.SetVibration(0, 0f, 0f);
+        yield return new WaitForSeconds(1);
+        GamePad.SetVibration(0, 1f, 1f);
+        yield return new WaitForSeconds(1);
+        GamePad.SetVibration(0, 0f, 0f);
+    }
+    IEnumerator Start()
+    {
+        print("Starting " + Time.time);
+
+        // Start function WaitAndPrint as a coroutine
+        yield return StartCoroutine("WaitAndPrint");
+        print("Done " + Time.time);
+    }
 }
