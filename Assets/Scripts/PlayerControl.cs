2a43c50138da89fc128111faaffc240f4fa9120d
Alexandre Muzio <ax.muzio@gmail.com>
2015-08-22 02:55:20 -0300

    Adding Missing InputManager components

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System;
 
+[Serializable]
 public class PlayerControl : IControl
 {
     private int _index;
+    [SerializeField]
+    private float dead_zone = 0.3f;
 
     public PlayerControl (int index)
     {
@@ -15,6 +18,16 @@ public class PlayerControl : IControl
         float x = Input.GetAxisRaw("Joystick" + _index + "XAxis");
         float y = Input.GetAxisRaw("Joystick" + _index + "YAxis");
 
+        //Debug.Log("Joystick" + _index + "XAxis");
+        //Debug.Log("Joystick" + _index + "YAxis");
+
+        //Debug.Log("index " + _index + ": " + x + " " +  y);
+
+        if (x*x + y*y < dead_zone)
+        {
+            return Vector2.zero;
+        }
+
         return new Vector2(x, y).normalized;
     }
 
@@ -25,6 +38,14 @@ public class PlayerControl : IControl
 
     public Vector2 GetAim()
     {
-        throw new NotImplementedException();
+        float x = Input.GetAxisRaw("Joystick" + _index + "AimXAxis");
+        float y = Input.GetAxisRaw("Joystick" + _index + "AimYAxis");
+
+        if (x * x + y * y < dead_zone)
+        {
+            return Vector2.zero;
+        }
+
+        return new Vector2(x, y).normalized;
     }
 }
